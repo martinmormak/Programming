6575fed15fb522675803a3824c268f139015cb84
Martin Mormák <martin.mormak@student.tuke.sk>
2022-02-28 18:10:06 +0100

    28.2 18:10

--- ps1/main.c
+++ ps1/main.c
@@ -7,38 +7,48 @@
 int main(){
     char *encrypted;
     //encrypted = playfair_encrypt("dote", "optometry");
-    encrypted = playfair_encrypt("frogs", "Kto sa skorej wrati nech wytiahne noz z bratowho mena a ked wyjde krw moze wediet ze mu je brat ziwy ale ak potecie woda istotne je mrtwy ");
+    encrypted = playfair_encrypt("Ked ti zima pod sa zohriat zavola ten zdola", "A mladenec mu rozpovedal ze je to taka a taka zakliata hora a kto sa raz do nej dostane ten stadial nevyjde viacej");
     printf("%s", encrypted);
     printf("\n");
     char *decrypted;
-    decrypted = playfair_decrypt("frogs", encrypted);
+    decrypted = playfair_decrypt("Ked ti zima pod sa zohriat zavola ten zdola", encrypted);
     printf("%s", decrypted);
     printf("\n");
+    free(decrypted);
+    free(encrypted);
     char* reversed = reverse("NULL"/*"Hello world!"*/);
     printf("%s", reversed);
     printf("\n");
-    encrypted = vigenere_encrypt("CoMPuTeR", "Hello world!");
-    printf("%s\n", encrypted);
-    decrypted = vigenere_decrypt("CoMPuTeR", encrypted);
-    printf("%s\n", decrypted);
-    unsigned char* encrypted2;
-    encrypted2 = bit_encrypt("Hello world!");
+    free(reversed);
+    char* encrypted2;
+    encrypted2 = vigenere_encrypt("CoMPuTeR", "Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!");
+    printf("%s\n", encrypted2);
+    char* decrypted2;
+    decrypted2 = vigenere_decrypt("CoMPuTeR", encrypted2);
+    printf("%s\n", decrypted2);
+    free(encrypted2);
+    free(decrypted2);
+    unsigned char* encrypted3;
+    encrypted3= bit_encrypt("Hello world!");
     for(int i=0; i < 12;i++) {
-        printf("%x ", encrypted2[i]);
+        printf("%x ", encrypted3[i]);
     }
     printf("\n");
-    decrypted = bit_decrypt(encrypted2);
-    printf("%s",decrypted);
+    char* decrypted3;
+    decrypted3 = bit_decrypt(encrypted3);
+    printf("%s",decrypted3);
     printf("\n");
-    encrypted2 = bmp_encrypt("CoMPuTeRCOMPUTERCOMPUTERCOMPUTERCOMPUTERCOMPUTERCOMPUTER", "Hello world!");
+    free(encrypted3);
+    free(decrypted3);
+    unsigned char* encrypted4;
+    encrypted4 = bmp_encrypt("CoMPuTeRCOMPUTERCOMPUTERCOMPUTERCOMPUTERCOMPUTERCOMPUTER", "Hello world!");
     for(int i=0; i < 12;i++) {
-        printf("%x ", encrypted2[i]);
+        printf("%x ", encrypted4[i]);
     }
     printf("\n");
-    decrypted = bmp_decrypt("CoMPuTeR", encrypted2);
-    printf("%s\n", decrypted);
-    //free(encrypted);
-    //free(encrypted2);
-    //free(decrypted);
-    //free(reversed);
+    char* decrypted4;
+    decrypted4 = bmp_decrypt("CoMPuTeR", encrypted4);
+    printf("%s\n", decrypted4);
+    free(encrypted4);
+    free(decrypted4);
 }
