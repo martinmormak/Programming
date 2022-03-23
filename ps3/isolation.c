9c12462f8c89f4486155229e7dcdff7e41c1ee77
Martin Mormák <martin.mormak@student.tuke.sk>
2022-03-23 11:11:34 +0100

    23.3 11:11

--- ps3/isolation.c
+++ ps3/isolation.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int cmp(const void *n1,const void *n2)
+{
+    return ( *(int*)n1 - *(int*)n2 );
+}
 
 int main()
 {
@@ -6,39 +13,94 @@ int main()
     int k;
     scanf("%d %d\n",&n,&k);
     unsigned long long array [n][k];
-    int retur [k*2-1];
+    int retur [2][n];
     for(int i=0;i<n;i++)
     {
         for(int x=0;x<k;x++)
         {
             if(x<k-1)
             {
-                scanf("%lld ",&array[i][k]);
+                scanf("%lld ",&array[i][x]);
             }
             else
             {
-                scanf("%lld\n",&array[i][k]);
+                scanf("%lld\n",&array[i][x]);
             }
         }
     }
-    int m=0;
-    int v=0;
+    /*for(int i=0;i<n;i++)
+    {
+        for(int x=0;x<k;x++)
+        {
+            printf("%lld\t",array[i][x]);
+        }
+        printf("\n");
+    }*/
     for(int i=0;i<n;i++)
     {
+        int m=1000;
+        int v=1000;
+        int sum=0;
         for(int x=1;x<k;x++)
         {
-            if(array[n][x]<array[n][0])
+            if(array[i][x]<array[i][0])
             {
-                m=1;
+                //printf("%lld<%lld\n",array[i][x],array[i][0]);
+                m++;
             }
-            else if(array[n][x]>array[n][0])
+            else if(array[i][x]>array[i][0])
             {
-                v=0;
+                //printf("%lld>%lld\n",array[i][x],array[i][0]);
+                v++;
             }
         }
-        if(m==1&&v==1)
+        //printf("%d\t%d\n",m,v);
+        retur[0][i]=m;
+        retur[1][i]=v;
+        if(m!=1000&&v!=1000)
         {
-            retur[0]++;
+        }
+        else
+        {
+            for(int x=1;x<k;x++)
+            {
+                if(array[i][x]<array[i][x-1])
+                {
+                    sum=sum-(int)pow(2,x);
+                }
+                else if(array[i][x]>array[i][x-1])
+                {
+                    sum=sum+(int)pow(2,x);
+                }
+            }
+            retur[0][i]=sum;
+            retur[1][i]=0;
         }
     }
+    for(int i=0;i<n;i++)
+    {
+        for(int x=1;x<n;x++)
+        {
+            if(retur[0][x]<retur[0][x-1])
+            {
+                int m=retur[0][x];
+                int v=retur[1][x];
+                retur[0][x]=retur[0][x-1];
+                retur[1][x]=retur[1][x-1];
+                retur[0][x-1]=m;
+                retur[1][x-1]=v;
+            }
+        }
+    }
+    //qsort(retur,(size_t)n,sizeof (int),cmp);
+    int v=1;
+    for(int i=1;i<n;i++)
+    {
+        if(retur[0][i]!=retur[0][i-1])
+        {
+            v++;
+        }
+        //printf("%d\t%d\n",retur[0][i],retur[1][i]);
+    }
+    printf("%d\n",v);
 }
--- ps3/racer.c
+++ ps3/racer.c
@@ -1,19 +1,46 @@
 #include <stdio.h>
+#include <math.h>
+#include <time.h>
 
 int main()
 {
-    unsigned long long number=0;
-    unsigned long long sum=0;
-    scanf("%lld",&number);
+    clock_t ti;
+    ti = clock();
+    int p;
+    float t;
+    scanf("%d %f\n",&p,&t);
+    float v[p];
+    float s[p];
+    for(int i=0;i<p;i++)
+    {
+        scanf("%f %f\n",&s[i],&v[i]);
+    }
+    float min=v[0];
+    for(int i=1;i<p;i++)
+    {
+        if(min>v[i])
+        {
+            min=v[i];
+        }
+    }
+    min=min*-1;
+    min=min+(float)pow(10,-6);
+    float vysledok=0;
     do
     {
-        sum=0;
-        do
+        vysledok=0;
+        for(int i=0;i<p;i++)
         {
-            sum=sum+number%10;
-            number=number/10;
-        }while(number>=1);
-        number=sum;
-    }while(sum>9);
-    printf("%lld\n",sum);
+            vysledok=vysledok+(s[i]/(v[i]+min));
+        }
+        min=min+(float)pow(10,-6);
+        printf("%f\t%f\n",min,vysledok);
+    }while(t!=vysledok);
+    min=min-(float)pow(10,-6);
+    printf("%f\n",min);
+    
+    ti = clock() - ti;
+    double time_taken = ((double)ti)/CLOCKS_PER_SEC; // in seconds
+  
+    printf("fun() took %f seconds to execute \n", time_taken);
 }
